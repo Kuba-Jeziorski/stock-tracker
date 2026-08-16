9d913c772869c65ff31eae5bcb80186d8d43a25b
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2026-08-16 15:54:31 +0100

    chore: added a skeleton to the stock list cells

--- src/features/stock-list/presentation/stock-list.tsx
+++ src/features/stock-list/presentation/stock-list.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Skeleton, Stack, Typography } from "@mui/material";
 import type { DetailedStock } from "../domain/models";
 
 type Props = {
@@ -64,61 +64,118 @@ export const StockList = ({ stocksPerPage, isFetching }: Props) => {
                 borderColor: "custom.table.separator",
               }}
             >
-              <Typography
+              <Box
                 sx={{
-                  display: "flex",
-                  width: 1,
                   paddingX: 4,
                   paddingY: 2,
-                  fontWeight: 600,
+                  width: 1,
                 }}
               >
-                {isFetching ? "..." : stock.ticker}
-              </Typography>
-              <Typography
-                sx={{ display: "flex", width: 1, paddingX: 4, paddingY: 2 }}
-              >
-                {isFetching ? "..." : stock.name}
-              </Typography>
-              <Typography
+                {isFetching ? (
+                  <Skeleton
+                    variant="rectangular"
+                    animation="pulse"
+                    width={210}
+                    height={24}
+                  />
+                ) : (
+                  <Typography
+                    sx={{
+                      display: "flex",
+                      width: 1,
+                      fontWeight: 600,
+                    }}
+                  >
+                    {stock.ticker}
+                  </Typography>
+                )}
+              </Box>
+              <Box
                 sx={{
-                  display: "flex",
-                  width: 1,
                   paddingX: 4,
                   paddingY: 2,
-                  fontWeight: 600,
+                  width: 1,
                 }}
               >
-                {isFetching
-                  ? "..."
-                  : stock.price === null
-                    ? "N/A"
-                    : `$${stock.price}`}
-              </Typography>
-              <Typography
+                {isFetching ? (
+                  <Skeleton
+                    variant="rectangular"
+                    animation="pulse"
+                    width={210}
+                    height={24}
+                  />
+                ) : (
+                  <Typography
+                    sx={{
+                      display: "flex",
+                      width: 1,
+                    }}
+                  >
+                    {stock.name}
+                  </Typography>
+                )}
+              </Box>
+              <Box
                 sx={{
-                  display: "flex",
-                  width: 1,
                   paddingX: 4,
                   paddingY: 2,
-                  fontWeight: 600,
-                  color: (theme) => {
-                    if (stock.change == null || stock.change === 0) {
-                      return theme.palette.custom.text.primary;
-                    }
+                  width: 1,
+                }}
+              >
+                {isFetching ? (
+                  <Skeleton
+                    variant="rectangular"
+                    animation="pulse"
+                    width={210}
+                    height={24}
+                  />
+                ) : (
+                  <Typography
+                    sx={{
+                      display: "flex",
+                      width: 1,
+                      fontWeight: 600,
+                    }}
+                  >
+                    {stock.price === null ? "N/A" : `$${stock.price}`}
+                  </Typography>
+                )}
+              </Box>
+              <Box
+                sx={{
+                  paddingX: 4,
+                  paddingY: 2,
+                  width: 1,
+                }}
+              >
+                {isFetching ? (
+                  <Skeleton
+                    variant="rectangular"
+                    animation="pulse"
+                    width={210}
+                    height={24}
+                  />
+                ) : (
+                  <Typography
+                    sx={{
+                      display: "flex",
+                      width: 1,
+                      fontWeight: 600,
+                      color: (theme) => {
+                        if (stock.change == null || stock.change === 0) {
+                          return theme.palette.custom.text.primary;
+                        }
 
-                    return stock.change > 0
-                      ? theme.palette.custom.status.positive
-                      : theme.palette.custom.status.negative;
-                  },
-                }}
-              >
-                {isFetching
-                  ? "..."
-                  : stock.change == null
-                    ? "N/A"
-                    : `${stock.change}%`}
-              </Typography>
+                        return stock.change > 0
+                          ? theme.palette.custom.status.positive
+                          : theme.palette.custom.status.negative;
+                      },
+                    }}
+                  >
+                    {stock.change == null ? "N/A" : `${stock.change}%`}
+                  </Typography>
+                )}
+              </Box>
             </Box>
           );
         })}
